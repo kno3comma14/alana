4fa3412157b30b0c8e7fcb82d67120aee9b4a5b9
kigenizer <kigenizer@gmail.com>
2021-06-23 15:55:58 -0500

    Finished setup for integration tests

--- test/clj_datastore/integration/environment.clj
+++ test/clj_datastore/integration/environment.clj
@@ -1,39 +1,35 @@
 (ns clj-datastore.integration.environment
   (:require [clj-datastore.operations :refer :all]))
 
-(def generated-data [{:name "Alana"  :gender "F" :created (now) :first-chapter-appearance 1}
-                     {:name "Marko"  :gender "M" :created (now) :first-chapter-appearance 1}
-                     {:name "Hazel"  :gender "F" :created (now) :first-chapter-appearance 1}
-                     {:name "Izabel" :gender "F" :created (now) :first-chapter-appearance 2}
-                     {:name "Klara"  :gender "F" :created (now) :first-chapter-appearance 6}
-                     {:name "Barr"   :gender "M" :created (now) :first-chapter-appearance 6}
-                     {:name "Yuma"   :gender "F" :created (now) :first-chapter-appearance 15}])
+(def entity-keys (atom []))
+
+(def generated-data [{:name "Alana"  :gender "F" :first-chapter-appearance 1}
+                     {:name "Marko"  :gender "M" :first-chapter-appearance 1}
+                     {:name "Izabel" :gender "F" :first-chapter-appearance 2}
+                     {:name "Klara"  :gender "F" :first-chapter-appearance 6}
+                     {:name "Barr"   :gender "M" :first-chapter-appearance 6}
+                     {:name "Yuma"   :gender "F" :first-chapter-appearance 15}])
 
 (def datastore (create-datastore))
 
 (def kind "some-saga-characters")
 
-(def entities 
-  (reduce (fn [acc, x]
+(def entities
+  (reduce (fn [acc, x] 
             (conj acc (create-entity datastore kind x)))
           []
           generated-data))
-
-(defn- now
-  []
-  (java.time.LocalDateTime/now))
 
 (defn init-gcp-environment
   "This function setup the initial environment for integration tests"
   []
   (doseq [entity entities]
-    (upsert-entity datastore entity)))
+    (swap! entity-keys conj (.getKey (upsert-entity datastore entity)))))
 
 (defn teardown-gcp-environment
   "This function teardown the initial environment for integration tests"
   []
-  (doseq [entity entities]
-    (.delete datastore entity)))
+  (.delete datastore (into-array @entity-keys)))
 
 (defn wrap-setup
   "Wrapper to be used for test fixtures"
--- test/clj_datastore/integration/operations_test.clj
+++ test/clj_datastore/integration/operations_test.clj
@@ -1,6 +1,7 @@
 (ns clj-datastore.integration.operations-test
   (:require [clojure.test :refer :all]
-            [clj-datastore.operations :refer :all])
+            [clj-datastore.operations :refer :all]
+            [clj-datastore.integration.environment :refer :all])
   (:import (com.google.cloud.datastore Datastore
                                        DatastoreOptions
                                        Entity
@@ -10,39 +11,44 @@
                                        StructuredQuery$PropertyFilter
                                        StructuredQuery$CompositeFilter)))
 
-(def integration-test-kind "test-kind")
-(def test-datastore (.getService (DatastoreOptions/getDefaultInstance)))
-(def test-key (.newKey (.setKind (.newKeyFactory test-datastore) integration-test-kind)))
+(use-fixtures :once wrap-setup)
 
+(def integration-test-kind "some-saga-characters")
+
+(def test-datastore (.getService (DatastoreOptions/getDefaultInstance)))
+
+(def test-key (.newKey (.setKind (.newKeyFactory test-datastore) integration-test-kind)))
 
 (deftest run-query-test
   (testing "Integration test to run a single filter query against a datastore and verify its type and values correctness"
     (let [ds test-datastore
           input-kind integration-test-kind
-          property-map [{:key "property1" :value "Hello" :type "="}]
+          property-map [{:key "name" :value "Alana" :type "="}]
           test-value (run-query ds input-kind property-map)
+          test-name (.get (.get (.getProperties (.next test-value)) "name"))
           expected-type com.google.cloud.datastore.QueryResults
-          expected-value "Hello"]
+          expected-value "Alana"]
       (is (isa? (type test-value) expected-type))
-      (is (=  (.get (.get (.getProperties (.next test-value)) "property1")) expected-value))))
+      (is (=  test-name expected-value))))
   (testing "Integration test to run a composite filter query against a datastore and verify its type and values correctness"
     (let [ds test-datastore
           input-kind integration-test-kind
-          property-map [{:key "property2" :value "World" :type "="}
-                        {:key "quantity" :value 32 :type "="}]
+          property-map [{:key "gender" :value "F" :type "="}
+                        {:key "first-chapter-appearance" :value 6 :type "="}]
           test-value (run-query ds input-kind property-map)
           expected-type com.google.cloud.datastore.QueryResults
-          expected-value "Hello"]
+          expected-value "Klara"]
       (is (isa? (type test-value) expected-type))
-      (is (=  (.get (.get (.getProperties (.next test-value)) "property1")) expected-value)))))
+      (is (=  (.get (.get (.getProperties (.next test-value)) "name")) expected-value)))))
 
 (deftest upsert-entity-test
   (testing "Integration test to upsert an entity into a datastore"
     (is
      (let [datastore test-datastore
            kind integration-test-kind
-           property-map {:property1 "Hello1" :property2 "Hello2" :quantity 33}
+           property-map {:name "Hazel"  :gender "F" :first-chapter-appearance 1}
            entity (create-entity datastore kind property-map)
            test-value (upsert-entity datastore entity)
            expected-type com.google.cloud.datastore.Entity]
+       (swap! entity-keys conj (.getKey test-value))
        (isa? (type test-value) expected-type)))))
